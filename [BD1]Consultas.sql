833fc0e10b679dca11a59e1606723abea2cae07b
HENRRY DAVID BRAN VELASQUEZ <2168036320108@ingenieria.usac.edu.gt>
2022-09-16 00:34:15 -0600

    Actualización de Consultas
    Terminamos las consultas a falta de revisión

--- [BD1]Consultas.sql
+++ [BD1]Consultas.sql
@@ -112,8 +112,125 @@ SELECT * FROM
 ORDER BY Total ASC;
 
 #-------------------------------------------------------------------------
-#6.MOSTRAR LA CATEGORIA QUE MAS Y MENOS SE HA COMPRADO 
-#
+#6.MOSTRAR LA CATEGORIA QUE MAS Y MENOS SE HA COMPRADO. DEBE DE MOSTRAR 
+#EL NOMBRE DE LA CATEGORIA Y CANTIDAD DE UNIDADES
 #-------------------------------------------------------------------------
+(SELECT Categoria.Descripcion,SUM(Cantidad) AS No_Articulos,SUM(Cantidad*Precio) AS Total
+FROM Categoria
+INNER JOIN Producto
+ON Categoria.Id_Categoria = Producto.Id_Categoria
+INNER JOIN Detalle_Factura
+ON Producto.Id_Producto = Detalle_Factura.Id_Producto
+GROUP BY Categoria.Descripcion
+ORDER BY No_Articulos DESC
+LIMIT 1)
+UNION
+(SELECT Categoria.Descripcion,SUM(Cantidad) AS No_Articulos,SUM(Cantidad*Precio) AS Total
+FROM Categoria
+INNER JOIN Producto
+ON Categoria.Id_Categoria = Producto.Id_Categoria
+INNER JOIN Detalle_Factura
+ON Producto.Id_Producto = Detalle_Factura.Id_Producto
+GROUP BY Categoria.Descripcion
+ORDER BY No_Articulos ASC
+LIMIT 1);
+
+#-------------------------------------------------------------------------
+#7.MOSTRAR LA CATEGORIA QUE MAS Y MENOS SE HA COMPRADO DE CADA PAIS
+#SE DEBE DE MOSTRAR NOMBRE DEL PAIS,NOMBRE DE LA CATEGORIA Y CANTIDAD 
+#DE UNIDADES
+#-------------------------------------------------------------------------
+SELECT Descripcion,Pais,MAX(No_Articulos) AS No_MAX_Articulos
+FROM (
+	SELECT Categoria.Descripcion,Pais.Nombre AS Pais,SUM(Cantidad) as No_Articulos
+	FROM Categoria
+	INNER JOIN Producto
+	ON Categoria.Id_Categoria = Producto.Id_Categoria
+	INNER JOIN Detalle_Factura
+	ON Producto.Id_Producto = Detalle_Factura.Id_Producto
+	INNER JOIN Factura
+	ON Detalle_Factura.Id_Factura = Factura.Id_Factura
+	INNER JOIN Cliente 
+	ON Factura.Id_Cliente = Cliente.Id_Cliente
+	INNER JOIN Pais
+	ON Cliente.Id_Pais = Pais.Id_Pais
+	GROUP BY Categoria.Descripcion,Pais.Nombre
+	ORDER BY Pais.Nombre,SUM(Cantidad) DESC) AS SUBCON
+WHERE Pais IN (
+    SELECT Nombre FROM Pais)
+GROUP BY Pais
+ORDER BY Pais,No_MAX_Articulos ASC;
+/*
+SELECT Categoria.Descripcion,Pais.Nombre AS Pais,SUM(Cantidad),SUM(Cantidad*Precio) AS Total
+FROM Categoria
+INNER JOIN Producto
+ON Categoria.Id_Categoria = Producto.Id_Categoria
+INNER JOIN Detalle_Factura
+ON Producto.Id_Producto = Detalle_Factura.Id_Producto
+INNER JOIN Factura
+ON Detalle_Factura.Id_Factura = Factura.Id_Factura
+INNER JOIN Cliente 
+ON Factura.Id_Cliente = Cliente.Id_Cliente
+INNER JOIN Pais
+ON Cliente.Id_Pais = Pais.Id_Pais
+GROUP BY Categoria.Descripcion,Pais.Nombre
+order by Pais.Nombre, SUM(Cantidad);
+*/
+
+#-------------------------------------------------------------------------
+#8.MOSTRAR LAS VENTAS POR MES DE INGLATERRA. DEBE MOSTRAR EL NUMERO DE MES
+#Y MONTO.
+#-------------------------------------------------------------------------
+SELECT MONTH(Fecha) AS Mes,SUM(Cantidad*Precio) AS Total
+FROM Producto
+INNER JOIN Detalle_Factura
+ON Producto.Id_Producto = Detalle_Factura.Id_Producto
+INNER JOIN Factura
+ON Detalle_Factura.Id_Factura = Factura.Id_Factura
+INNER JOIN Cliente 
+ON Factura.Id_Cliente = Cliente.Id_Cliente
+INNER JOIN Pais
+ON Cliente.Id_Pais = Pais.Id_Pais
+WHERE Pais.Nombre LIKE 'Inglaterra\r'
+GROUP BY Mes,Pais.Nombre
+ORDER BY Pais.Nombre,Mes;
+
+#-------------------------------------------------------------------------
+#9.MOSTRAR EL MES CON MAS Y MENOS VENTAS. SE DEBE MOSTRAR EL NUMERO DE 
+#MES Y MONTO.(EN UNA SOLA CONSULTA)
+#-------------------------------------------------------------------------
+(SELECT MONTH(Fecha) AS Mes,SUM(Cantidad*Precio) AS Total
+FROM Producto
+INNER JOIN Detalle_Factura
+ON Producto.Id_Producto = Detalle_Factura.Id_Producto
+INNER JOIN Factura
+ON Detalle_Factura.Id_Factura = Factura.Id_Factura
+GROUP BY Mes
+ORDER BY Total DESC
+LIMIT 1)
+UNION
+(SELECT MONTH(Fecha) AS Mes,SUM(Cantidad*Precio) AS Total
+FROM Producto
+INNER JOIN Detalle_Factura
+ON Producto.Id_Producto = Detalle_Factura.Id_Producto
+INNER JOIN Factura
+ON Detalle_Factura.Id_Factura = Factura.Id_Factura
+GROUP BY Mes
+ORDER BY Total ASC
+LIMIT 1);
+
+#-------------------------------------------------------------------------
+#10.MOSTRAR LAS VENTAS DE CADA PRODUCTO DE LA CATEGORIA DEPORTES. 
+#SE DEBE DE MOSTRAR EL ID DEL PRODUCTO, NOMBRE Y MONTO
+#-------------------------------------------------------------------------
+SELECT Producto.Id_Producto,Producto.Nombre,sum(Cantidad) AS No_Articulos,Precio,SUM(Cantidad*Precio) AS Total
+FROM Categoria
+INNER JOIN Producto
+ON Categoria.Id_Categoria = Producto.Id_Categoria
+INNER JOIN Detalle_Factura
+ON Producto.Id_Producto = Detalle_Factura.Id_Producto
+WHERE Categoria.Descripcion LIKE 'Deportes\r'
+GROUP BY Producto.Id_Producto,Producto.Nombre
+ORDER BY  No_Articulos, Producto.Nombre DESC;
 
 
